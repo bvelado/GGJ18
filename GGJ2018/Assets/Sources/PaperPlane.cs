e429733dcb01d5f67017432bc9b48124d4ba8d03
Alexis Girard <agirard@apperture.fr>
2018-01-27 11:01:00 +0100

    Added position update on mass change

--- GGJ2018/Assets/Sources/PaperPlane.cs
+++ GGJ2018/Assets/Sources/PaperPlane.cs
@@ -13,11 +13,13 @@ public class PaperPlane : MonoBehaviour {
     float _minCurveTime;
 
     Vector3 _initialScale;
+    Vector3 _initialPosition;
 
     private void Start()
     {
         _currentMass = _initialMass;
         _initialScale = transform.localScale;
+        _initialPosition = transform.localPosition;
         _maxCurveTime = _massEvolution.keys[_massEvolution.length - 1].time;
         _minCurveTime = _massEvolution.keys[0].time;
     }
@@ -27,7 +29,7 @@ public class PaperPlane : MonoBehaviour {
         _currentTime -= Time.deltaTime;
         _currentTime = Mathf.Max(_currentTime, _minCurveTime);
         _currentMass = _initialMass + _massEvolution.Evaluate(_currentTime);
-        UpdateScale();
+        ReactToMassChange();
         Debug.Log(string.Format("[TakePaper/{0}] - Current mass : {1}", gameObject.name, _currentMass));
     }
 
@@ -36,12 +38,14 @@ public class PaperPlane : MonoBehaviour {
         _currentTime += Time.deltaTime;
         _currentTime = Mathf.Min(_currentTime, _maxCurveTime);
         _currentMass = _initialMass + _massEvolution.Evaluate(_currentTime);
-        UpdateScale();
+        ReactToMassChange();
         Debug.Log(string.Format("[GivePaper/{0}] - Current mass : {1}", gameObject.name, _currentMass));
     }
 
-    void UpdateScale()
+    void ReactToMassChange()
     {
+        var massEvolution = _massEvolution.Evaluate(_currentTime);
         transform.localScale = new Vector3(_currentMass, _currentMass, _currentMass);
+        transform.localPosition = _initialPosition + Vector3.up * massEvolution;
     }
 }

2e57007ea4340978e04aabe3edd4b95cb875122d
Baptiste Velado <bvelado@apperture.fr>
2018-01-27 15:21:01 +0100

    WIP

---
+++ GGJ2018/Assets/Sources/Obstacles/Obstacle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(BoxCollider))]
+public class Obstacle : MonoBehaviour {
+
+	private void Reset(){
+		gameObject.isStatic = true;
+		GetComponent<BoxCollider>().isTrigger = true;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log(other.name);
+	}
+
+}
